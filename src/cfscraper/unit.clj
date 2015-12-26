1c1f3d7c6979aec5af0e569bdc2bb2772d53341d
Ryan Miller <rozifus@gmail.com>
2015-12-26 14:30:43 +1100

    pulling artist pages

--- src/cfscraper/unit.clj
+++ src/cfscraper/unit.clj
@@ -1,5 +1,6 @@
 (ns cfscraper.unit
-  (:import (com.gargoylesoftware.htmlunit WebClient BrowserVersion))
+  (:import (com.gargoylesoftware.htmlunit 
+             WebClient BrowserVersion NicelyResynchronizingAjaxController))
   (:use [clojure.string :only [upper-case]]))
 
 (def ^:dynamic *client*)
@@ -15,14 +16,16 @@
     (System/exit 0)))
 (println config)
 
-
 (def base-url "http://customsforge.com/")
 (def login-url "http://customsforge.com/index.php?app=core&module=global&section=login")
+(def artists-url "http://ignition.customsforge.com/search/artists")
+
+(def artist-list-xpath "//*[@id='artists-column']/a/@href")
 
 (def login-form-id "login")
-(def username-field-id "ips_username")
-(def password-field-id "ips_password")
-(def login-submit-class "input_submit")
+(def username-field-name "ips_username")
+(def password-field-name "ips_password")
+(def login-submit-value "Sign In")
 
 (def ^:dynamic *current-url* base-url)
 
@@ -30,51 +33,38 @@
   ([]
      (make-client (BrowserVersion/getDefault)))
   ([version]
-       (println (str "Browser: " (.getNickname version)))
-       (new WebClient version)))
+     (println (str "Browser: " (.getNickname version)))
+     (let [client (WebClient. version)]
+       (.setAjaxController client (NicelyResynchronizingAjaxController.))
+       (-> client (.getOptions) (.setThrowExceptionOnScriptError false))
+       client)))
 
 (defn get-page
   [client url]
   (.getPage client url))
 
-(defn get-nodes-by-xpath
-  [node xpath]
-  (.getByXPath node xpath))
+(defn get-login-form [page]
+  (.getElementById page "login"))
 
-(defn get-first-node-by-xpath
-  [node xpath]
-  (.getFirstByXPath node xpath))
+(defn login-via-form [login-form]
+  (let [username-field (.getInputByName login-form username-field-name)
+        password-field (.getInputByName login-form password-field-name)
+        submit-button (.getInputByValue login-form login-submit-value)]
+    (.setValueAttribute username-field (:username config))
+    (.setValueAttribute password-field (:password config))
+    (.click submit-button)))
 
-(defn get-node-anchors
-  [node]
-  (get-nodes-by-xpath node "//a"))
+(defn log-in [client]
+  (if-let [login-form (get-login-form (get-page client base-url))]
+      (login-via-form login-form)))
 
-(defn get-nodes-anchors
-  [nodes]
-  (flatten (map #(get-node-anchors %) nodes))) 
-
-(defn get-node-attributes
-  [node]
-  (let [attrs (.getAttributes node)
-        length (.getLength attrs)
-	items (map #(.item attrs %) (range 0 length))
-	hash (reduce #(merge %1 {(keyword (.getName %2)) (.getValue %2)}) {} items)]
-    hash))
-
-(defn has-login-button? [page]
-  (.getFirstByXPath page (str "//a[@href='" login-url "']")))
-
-(defn set-field [field-id value]
-  )
-
-(defn login-via-form [client]
-  (let [login-form (.getElementById client "login")]
-    (if login-form
-      (let [username-field (.getElementById "1")]))))
+(defn get-artist-url-list [client]
+  (let [page (get-page client artists-url)]
+    (map #(.getNodeValue %) (.getByXPath page artist-list-xpath))))
 
 (defn main []
-  (let [client (make-client BrowserVersion/FIREFOX_38)
-        page (get-page client base-url)]
-    (if (has-login-button? page)
-      (println (get-page client login-url)))))
-
+  (let [client (make-client BrowserVersion/FIREFOX_38)]
+    (log-in client)
+    (let [artist-pages (get-artist-url-list client)]
+      (println artist-pages))))
+  
