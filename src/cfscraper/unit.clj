83208642d702829d6a95a75ec289b5549c8178ee
Ryan Miller <rozifus@gmail.com>
2015-12-26 22:01:00 +1100

    run through pages

--- src/cfscraper/unit.clj
+++ src/cfscraper/unit.clj
@@ -1,6 +1,7 @@
 (ns cfscraper.unit
   (:import (com.gargoylesoftware.htmlunit 
-             WebClient BrowserVersion NicelyResynchronizingAjaxController))
+             WebClient BrowserVersion WebWindowListener
+             NicelyResynchronizingAjaxController))
   (:use [clojure.string :only [upper-case]]))
 
 (def ^:dynamic *client*)
@@ -18,9 +19,14 @@
 
 (def base-url "http://customsforge.com/")
 (def login-url "http://customsforge.com/index.php?app=core&module=global&section=login")
+(def search-url "http://ignition.customsforge.com")
 (def artists-url "http://ignition.customsforge.com/search/artists")
 
 (def artist-list-xpath "//*[@id='artists-column']/a/@href")
+(def song-list-xpath "//*/div[@id='groups-content']/div/div[2]/table/tbody/tr")
+(def search-button-xpath "//*[@id='search']")
+(def next-page-enabled-xpath 
+  "//*[@id='pag-next' and contains(concat(' ', @class, ' '), ' open ')]")
 
 (def login-form-id "login")
 (def username-field-name "ips_username")
@@ -39,9 +45,19 @@
        (-> client (.getOptions) (.setThrowExceptionOnScriptError false))
        client)))
 
+(defn parint [v]
+  (do
+    (println v)
+    v))
+
 (defn get-page
   [client url]
   (.getPage client url))
+
+(defn get-web-window-listener []
+  (proxy [WebWindowListener] []
+    (webWindowOpened [event] 
+      (println event)))) 
 
 (defn get-login-form [page]
   (.getElementById page "login"))
@@ -60,11 +76,32 @@
 
 (defn get-artist-url-list [client]
   (let [page (get-page client artists-url)]
-    (map #(.getNodeValue %) (.getByXPath page artist-list-xpath))))
+    (parint (map #(.getValue %) (.getByXPath page artist-list-xpath)))))
+
+(defn get-songs [client artist-url]
+  (let [x (parint artist-url)
+        artist-page (get-page client artist-url)]
+    (.getByXPath artist-page song-list-xpath)))
+
+(defn get-search-page [client]
+  (get-page client search-url))
+
+(defn get-songs [client page]
+  client)
+
+(defn get-next-button [page]
+  (.getFirstByXPath page next-page-enabled-xpath))
+
+(defn click-next-button [page]
+  (.click (get-next-button page)))
 
 (defn main []
   (let [client (make-client BrowserVersion/FIREFOX_38)]
     (log-in client)
-    (let [artist-pages (get-artist-url-list client)]
-      (println artist-pages))))
+    (loop [page (get-search-page client)]
+      (get-songs client page)
+      (if (get-next-button page)
+        (recur (click-next-button page))
+        (println "DONE")))))
+
   
